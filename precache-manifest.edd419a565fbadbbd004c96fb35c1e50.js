9aa00bd912284305ec74c89b11b8cd6d2dad9f3e
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-05-26 15:00:15 +0300

    Update

---
+++ precache-manifest.edd419a565fbadbbd004c96fb35c1e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e86b70f5b352b40188bf32cd5b47154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f08b217d5cb6e15d8105",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "9d3089222460edf94774",
+    "url": "/static/css/main.83837ee5.chunk.css"
+  },
+  {
+    "revision": "f08b217d5cb6e15d8105",
+    "url": "/static/js/2.0e392adf.chunk.js"
+  },
+  {
+    "revision": "325a1cb8fe50b59b2963b91d2c13d2e9",
+    "url": "/static/js/2.0e392adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3089222460edf94774",
+    "url": "/static/js/main.229c3fd0.chunk.js"
+  },
+  {
+    "revision": "eff894a85e67612247f6",
+    "url": "/static/js/runtime-main.77cced3a.js"
+  }
+]);
